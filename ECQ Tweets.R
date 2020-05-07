a57128850dbf0d1b6c21e19dba69c4c5381c8eb2
isidorachavez <isidorarosechavez@gmail.com>
2020-05-06 19:16:24 -0700

    Update ECQ Tweets.R

--- ECQ Tweets.R
+++ ECQ Tweets.R
@@ -5,6 +5,8 @@ library(tidytext)
 install.packages("stopwords")
 library("stopwords")
 
+#I want to see the sentiments on the Enhanced Community Quarantine in the Philippines during Covid19
+
 ###### TWITTER ######
 
 
@@ -14,7 +16,10 @@ consumer_secret <- 'bg1rQeTzRHLnwtsDflDEDzxioTg5tc4Qqq3OofyU8KTvkMs4oH'
 access_token <- '1510474681-43P0PJGtC0zkeALdIPg7xkiGU85YLtqYcvNvEdc'
 access_secret <- 'SnvddczcMJk7aWezu9tXkyMPtv1MrqWUYNAuITXigxAQL'
 
+#Select 1 after entering setup_twitter_oauth to authorize
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+
+#I get 1000 tweets that use #ecq starting on March 16, 2020
 ecq <- twitteR::searchTwitter('#ecq', n = 1000, since = '2020-03-16', retryOnRateLimit = 1e3)
 e = twitteR::twListToDF(ecq)
 
@@ -22,6 +27,7 @@ e = twitteR::twListToDF(ecq)
 
 e$text <- as.character(e$text)
 
+#These are to clean weird and repeating unnecessary parts of tweets
 e$text = gsub("&amp", "", e$text) 
 e$text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", e$text)
 e$text = gsub("@\\w+", "", e$text)
@@ -39,8 +45,9 @@ tagalog <- stopwords("tl", source = "stopwords-iso")
 tagalog_stop <- paste(tagalog, collapse = "|")
 
 #Tagalog stopwords are changed to a blank
+stopword2 <- paste0("\\b", tagalog_stop, "\\b")
+stopword3 <- paste(stopword2, collapse = "|")
 e$text <- gsub(pattern = stopword3, replacement = " ", x = e$text, ignore.case = TRUE)
-e
 
 #I create a tribble with other useless words that may appear
 custom_stop <- tribble(
@@ -53,7 +60,9 @@ custom_stop <- tribble(
   "lang","CUSTOM",
   "quarantine","CUSTOM",
   "gcq","CUSTOM",
-  "freethenipple","CUSTOM",)
+  "freethenipple","CUSTOM",
+  "ecq","CUSTOM",
+  "enhanced","CUSTOM") #this is removed as we are searching for "Enhanced Community Quarantine"
 
 ##### FREQUENCIES #####
 
@@ -63,6 +72,21 @@ ecq_token_freq <- e %>%
   anti_join(stop_words) %>% 
   anti_join(custom_stop) %>% 
   count(word, sort=TRUE)
+
+#This creates a bar plot of frequently used words
+freq_hist <- e %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words) %>%
+  count(word, sort=TRUE) %>%
+  top_n(10)%>%
+  mutate(word=reorder(word, n)) %>% #Mutate reorder based on frequency. and then im overwriting my variable
+  ggplot(aes(word, n))+
+  geom_col()+
+  xlab(NULL)+
+  coord_flip()
+
+#This prints out the bar plot. Unfortunately, it does not say much
+print(freq_hist)
 
 ##### BING ANALYSIS #####
 
